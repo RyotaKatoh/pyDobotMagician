ccbd040de1c25dbfb38268e5e720ad168a63d273
Ryota Katoh <l94q.developer@gmail.com>
2018-10-27 12:17:57 +0900

    feat: disconnect dobot when shutdown

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 from flask import Flask, jsonify, request
 from Dobot import Dobot
+import signal
+import argparse
+import sys
 
 import json
-
-dobot = Dobot()
 
 baseZ = 0
 upZ = 20
@@ -15,19 +16,23 @@ current_position = {
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def status():
     return jsonify({"status": "OK"})
+
 
 @app.route("/init", methods=["GET"])
 def init():
     dobot.setHOME()
     return jsonify({"status": "OK"})
 
+
 @app.route("/stop_all", methods=["GET"])
 def stop_all():
     dobot.stopQueue()
     return jsonify({"status": "stop"})
+
 
 @app.route("/move", methods=["POST"])
 def move():
@@ -55,6 +60,31 @@ def move():
     return jsonify({"index": idx, "current_position": current_position})
 
 
-
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--env", type=str, default="debug")
+    args = parser.parse_args()
+
+    production = False
+    if args.env == "production":
+        production = True
+
+    print("production env: {0}".format(production))
+
+    dobot = Dobot(production=production)
+
+
+    def defer(signal, frame):
+        global dobot
+        if production:
+            dobot.disconnect()
+            print("disconnect")
+        else:
+            print("disconnect")
+
+        sys.exit(0)
+
+
+    signal.signal(signal.SIGINT, defer)
+
     app.run(host="0.0.0.0", port=3001)
